1562371e0b8b4b7bff51d0ed34539faa5411714b
vita <gamarjatko@gmail.com>
2020-01-16 12:12:23 +0200

    first commit

---
+++ mongodb.js
@@ -0,0 +1,36 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+    if(error){
+        return console.log('unable to connect to db')
+    }
+    console.log('connected')
+    const db = client.db(databaseName)
+
+    /*db.collection('users').insertOne({
+        name: 'Vita',
+        age: 27
+    }, (error, res) => {
+        if(error) {
+            return console.log('Disable to insert')
+        }
+        console.log(res.ops)
+    })*/
+    /*db.collection('users').insertMany([{
+        name: 'Jane',
+        age: 28
+    },
+        {
+            name: 'Ganter',
+            age: 25
+        }], (error, result) => {
+            if(error){
+                return console.log('error')
+            }
+            console.log(result.ops)
+    })*/
+})
